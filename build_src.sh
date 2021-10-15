3cc5274f714fa1ebde6ab946b8dc87dfecc98ebc
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-10-15 12:53:13 +0000

    Ajout update common sur build

--- build_src.sh
+++ build_src.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "MAJ common"
+git submodule update --recursive
+
 echo "Build target rust"
 cargo b --release --package millegrilles_senseurspassifs --bin millegrilles_senseurspassifs
 
